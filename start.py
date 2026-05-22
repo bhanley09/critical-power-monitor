d5b68360729c11c4f8c5d0fbeedbd55b3d859d4f
Blake Hanley <b.hanley09@yahoo.com>
2026-05-22 00:34:51 -0400

    add root startup script

--- start.py
+++ start.py
@@ -1,9 +1,13 @@
 import subprocess
 import time
-import os
 
 subprocess.Popen(["python", "simulator/simulator.py"])
 
 time.sleep(3)
 
-subprocess.run(["gunicorn", "dashboard.app:server", "--bind", "0.0.0.0:10000"])
+subprocess.run([
+    "gunicorn",
+    "dashboard.app:server",
+    "--bind",
+    "0.0.0.0:10000"
+])
